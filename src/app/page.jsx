4d5dc5374be970252c78d21247fbbf400a2a3c8f
Ayushmaanagarwal1211 <118350936+Ayushmaanagarwal1211@users.noreply.github.com>
2024-03-21 13:54:44 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import Card from "../../components/Card";
 import MarketCards from "../../components/MarketCards";
 import { user, sideUser } from "./data.js";
-import SideBar from "../../components/SideBar";
+import { FaBell } from "react-icons/fa6";
 
 export default function Page(param) {
   const [showSideBar, setShowSideBar] = useState(false);
@@ -49,7 +49,24 @@ export default function Page(param) {
         
         {showSideBar && (
           <div className={`text-xl ${isMobile ? "w-[70%]" : "sidebarAnimate"} text-white h-[100vh] ${isMobile ? "absolute" : "relative"} bg-[#1b5d86] z-[100]`} key="sidebar">
-           <SideBar />
+             <div className="h-[10%] flex justify-center flex-row items-center" style={{ borderBottom: "solid white 1px" }}>
+              <p className="w-[75%]">Hello user</p>
+              <p><FaBell /></p>
+            </div>
+            <div className="h-[90%] flex flex-col w-[100%]">
+              <div className="h-[10%] pl-5 flex flex-row items-center gap-2 mb-5">
+                <p className="flex items-center flex-row">Discussion</p>
+                <p className="text-xl">
+                  <select className="bg-[#1b5d86]"></select>
+                </p>
+              </div>
+              <div className="h-[10%] pl-5" key="market">Market Stories</div>
+              <div className="h-[10%] pl-5" key="sentiment">Sentiment</div>
+              <div className="h-[10%] pl-5" key="mar">Market</div>
+              <div className="h-[10%] pl-5" key="watch">WatchLists</div>
+              <div className="h-[10%] pl-5" key="event">Events</div>
+              <div className="h-[10%] pl-5" key="news">News/Interview</div>
+            </div>
           </div>
         )}
         {showDiscussion && (
